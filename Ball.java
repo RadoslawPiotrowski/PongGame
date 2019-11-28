cc08f0fc3d50926a7f4b090adabb4c596188f87c
radziop <emirad00@gmail.com>
2019-11-28 11:14:04 +0100

    Add pong game

---
+++ Ball.java
@@ -0,0 +1,106 @@
+package com.example.pingpong;
+
+import android.graphics.RectF;
+
+import java.util.Random;
+
+public class Ball {
+
+    private RectF mRect;
+    private float mXVelocity;
+    private float mYVelocity;
+    private float mBallWidth;
+    private float mBallHeight;
+
+    public Ball(int screenX, int screenY){
+
+        // Make the mBall size relative to the screen resolution
+        mBallWidth = screenX / 100;
+        mBallHeight = mBallWidth;
+
+    /*
+        Start the ball travelling straight up
+        at a quarter of the screen height per second
+    */
+        mYVelocity = screenY / 8;
+        mXVelocity = mYVelocity / 2;
+
+        // Initialize the Rect that represents the mBall
+        mRect = new RectF();
+    }
+
+    // Give access to the Rect
+    public RectF getRect(){
+        return mRect;
+    }
+
+    // Change the position each frame
+    public void update(long fps){
+        mRect.left = mRect.left + (mXVelocity / fps);
+        mRect.top = mRect.top + (mYVelocity / fps);
+        mRect.right = mRect.left + mBallWidth;
+        mRect.bottom = mRect.top - mBallHeight;
+    }
+
+    // Reverse the vertical heading
+    public void reverseYVelocity(){
+        mYVelocity = -mYVelocity;
+    }
+
+    // Reverse the horizontal heading
+    public void reverseXVelocity(){
+        mXVelocity = -mXVelocity;
+    }
+
+    public void setRandomXVelocity(){
+
+        // Generate a random number either 0 or 1
+        Random generator = new Random();
+        int answer = generator.nextInt(2);
+
+        if(answer == 0){
+            reverseXVelocity();
+        }
+    }
+
+    // Speed up by 10%
+// A score of over 20 is quite difficult
+// Reduce or increase 10 to make this easier or harder
+    public void increaseVelocity(double mSpeed){
+        double mXAbsoluteVelocity = Math.abs(mXVelocity);
+        double mYAbsoluteVelocity = Math.abs(mYVelocity);
+        mXAbsoluteVelocity += mSpeed;
+        mYAbsoluteVelocity += mSpeed;
+        if (mXVelocity < 0){
+            mXVelocity = (float) (mXAbsoluteVelocity * (-1));
+        }
+        else{
+            mXVelocity = (float) (mXAbsoluteVelocity);
+        }
+        if (mYVelocity < 0){
+            mYVelocity = (float) (mYAbsoluteVelocity * (-1));
+        }
+        else{
+            mYVelocity = (float) (mYAbsoluteVelocity);
+        }
+    }
+
+    public void clearObstacleY(float y){
+        mRect.bottom = y;
+        mRect.top = y - mBallHeight;
+    }
+
+    public void clearObstacleX(float x){
+        mRect.left = x;
+        mRect.right = x + mBallWidth;
+    }
+
+    public void reset(int x, int y){
+        mRect.left = x / 2;
+        mRect.top = y - 300;
+        mRect.right = x / 2 + mBallWidth;
+        mRect.bottom = y - 300 - mBallHeight;
+    }
+
+
+}
---
+++ PongView.java
@@ -0,0 +1,279 @@
+package com.example.pingpong;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class PongView extends SurfaceView implements Runnable{
+
+    // This is our thread
+    Thread mGameThread = null;
+
+    // We need a SurfaceHolder object
+    // We will see it in action in the draw method soon.
+    SurfaceHolder mOurHolder;
+
+    // A boolean which we will set and unset
+    // when the game is running- or not
+    // It is volatile because it is accessed from inside and outside the thread
+    volatile boolean mPlaying;
+
+    // Game is mPaused at the start
+    boolean mPaused = true;
+
+    // A Canvas and a Paint object
+    Canvas mCanvas;
+    Paint mPaint;
+
+    // This variable tracks the game frame rate
+    long mFPS;
+
+    // The size of the screen in pixels
+    int mScreenX;
+    int mScreenY;
+
+    // The players mBat
+    Bat mBat;
+
+    // The players mBat
+    Bat mBatTwo;
+
+    // A mBall
+    Ball mBall;
+
+    // The mScore
+    int mScore = 0;
+
+    // Lives
+    int mLives = 3;
+
+    double mBallAcceleration = 0.01;
+    double mBallSpeedUp = 0;
+
+    public PongView(Context context, int x, int y) {
+
+    /*
+        The next line of code asks the
+        SurfaceView class to set up our object.
+    */
+        super(context);
+
+        // Set the screen width and height
+        mScreenX = x;
+        mScreenY = y;
+
+        // Initialize mOurHolder and mPaint objects
+        mOurHolder = getHolder();
+        mPaint = new Paint();
+
+        // A new mBat
+        mBat = new Bat(mScreenX, mScreenY, 0);
+
+        mBatTwo = new Bat(mScreenX, mScreenY, 1);
+
+        // Create a mBall
+        mBall = new Ball(mScreenX, mScreenY);
+
+
+        setupAndRestart();
+    }
+
+    public void setupAndRestart(){
+        // Put the mBall back to the start
+        mBall.reset(mScreenX, mScreenY);
+
+        // if game over reset scores and mLives
+        if(mLives == 0) {
+            mScore = 0;
+            mLives = 3;
+        }
+    }
+
+    @Override
+    public void run() {
+
+        while (mPlaying) {
+
+            // Capture the current time in milliseconds in startFrameTime
+            long startFrameTime = System.currentTimeMillis();
+
+            // Update the frame
+            if(!mPaused){
+                update();
+            }
+
+            // Draw the frame
+            draw();
+
+        /*
+            Calculate the FPS this frame
+            We can then use the result to
+            time animations in the update methods.
+        */
+            long timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if (timeThisFrame >= 1) {
+                mFPS = 1000 / timeThisFrame;
+            }
+        }
+    }
+    // Everything that needs to be updated goes in here
+// Movement, collision detection etc.
+    public void update() {
+
+        // Move the mBat if required
+        mBat.update(mFPS);
+
+        mBatTwo.update(mFPS);
+
+        mBall.update(mFPS);
+
+        mBallSpeedUp += mBallAcceleration;
+        mBall.increaseVelocity(mBallSpeedUp);
+
+        // Check for mBall colliding with mBat
+        if(RectF.intersects(mBat.getRect(), mBall.getRect())) {
+            mBall.setRandomXVelocity();
+            mBall.reverseYVelocity();
+            mBall.clearObstacleY(mBat.getRect().top - 2);
+
+            mScore++;
+        }
+
+        // Bounce the mBall back when it hits the bottom of screen
+        if(mBall.getRect().bottom > mScreenY){
+            mBall.reverseYVelocity();
+            mBall.clearObstacleY(mScreenY - 2);
+
+            // Lose a life
+            mLives--;
+            mBallSpeedUp = 0;
+
+            if(mLives == 0){
+                mPaused = true;
+                setupAndRestart();
+            }
+        }
+
+        // Bounce the mBall back when it hits the top of screen
+        if(mBall.getRect().top < 0){
+            mBall.reverseYVelocity();
+            mBall.clearObstacleY(12);
+        }
+
+        // If the mBall hits left wall bounce
+        if(mBall.getRect().left < 0){
+            mBall.reverseXVelocity();
+            mBall.clearObstacleX(2);
+        }
+
+        // If the mBall hits right wall bounce
+        if(mBall.getRect().right > mScreenX){
+            mBall.reverseXVelocity();
+            mBall.clearObstacleX(mScreenX - 22);
+        }
+    }
+
+    // Draw the newly updated scene
+    public void draw() {
+
+        // Make sure our drawing surface is valid or we crash
+        if (mOurHolder.getSurface().isValid()) {
+
+            // Draw everything here
+
+            // Lock the mCanvas ready to draw
+            mCanvas = mOurHolder.lockCanvas();
+
+            // Clear the screen with my favorite color
+            mCanvas.drawColor(Color.argb(255, 120, 197, 87));
+
+            // Choose the brush color for drawing
+            mPaint.setColor(Color.argb(255, 255, 255, 255));
+
+            // Draw the mBat
+            mCanvas.drawRect(mBat.getRect(), mPaint);
+
+            // Draw the mBat_Two
+            mCanvas.drawRect(mBatTwo.getRect(), mPaint);
+
+            // Draw the mBall
+            mCanvas.drawRect(mBall.getRect(), mPaint);
+
+
+            // Change the drawing color to white
+            mPaint.setColor(Color.argb(255, 255, 255, 255));
+
+            // Draw the mScore
+            mPaint.setTextSize(40);
+            mCanvas.drawText("Score: " + mScore + "   Lives: " + mLives, 10, 50, mPaint);
+
+            // Draw everything to the screen
+            mOurHolder.unlockCanvasAndPost(mCanvas);
+        }
+    }
+
+    // If the Activity is paused/stopped
+    // shutdown our thread.
+    public void pause() {
+        mPlaying = false;
+        try {
+            mGameThread.join();
+        } catch (InterruptedException e) {
+            Log.e("Error:", "joining thread");
+        }
+    }
+
+    // If the Activity starts/restarts
+    // start our thread.
+    public void resume() {
+        mPlaying = true;
+        mGameThread = new Thread(this);
+        mGameThread.start();
+    }
+
+
+    // The SurfaceView class implements onTouchListener
+// So we can override this method and detect screen touches.
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+
+            // Player has touched the screen
+            case MotionEvent.ACTION_DOWN:
+
+                mPaused = false;
+
+                // Is the touch on the right or left?
+                if((motionEvent.getX() > mScreenX / 2) && (motionEvent.getY() > mScreenY / 2)){
+                    mBat.setMovementState(mBat.RIGHT);
+                }
+                else if((motionEvent.getX() < mScreenX / 2) && (motionEvent.getY() > mScreenY / 2)){
+                    mBat.setMovementState(mBat.LEFT);
+                }
+
+                if((motionEvent.getX() > mScreenX / 2) && (motionEvent.getY() < mScreenY / 2)){
+                    mBatTwo.setMovementState(mBatTwo.RIGHT);
+                }
+                else if((motionEvent.getX() < mScreenX / 2) && (motionEvent.getY() < mScreenY / 2)){
+                    mBatTwo.setMovementState(mBatTwo.LEFT);
+                }
+                break;
+
+            // Player has removed finger from screen
+            case MotionEvent.ACTION_UP:
+
+                mBat.setMovementState(mBat.STOPPED);
+                mBatTwo.setMovementState(mBatTwo.STOPPED);
+                break;
+        }
+        return true;
+    }
+
+}
